7b2de0b79727415af97ecd971e66b017892c1380
takuyakanbr <takuyakanbr@gmail.com>
2018-02-12 21:55:27 +0800

    Printable: add static getPrintableString method ReadOnlyPerson: use getPrintableString within getAsTextShowAll

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -9,4 +9,18 @@ public interface Printable {
      * Returns a printable string representing this printable object.
      */
     String getPrintableString();
+
+    /**
+     * Returns a concatenated version of the printable strings of each object.
+     */
+    static String getPrintableString(Printable... printables) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < printables.length; ++i) {
+            builder.append(printables[i].getPrintableString());
+            if (i > 0) {
+                builder.append(" ");
+            }
+        }
+        return builder.toString();
+    }
 }
