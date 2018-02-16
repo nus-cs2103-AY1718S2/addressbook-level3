ea0abf487ab773cc3e5d68709f3f0eb3dac50621
chochihtun <33831669+ChoChihTun@users.noreply.github.com>
2018-02-16 22:51:12 +0800

    Create Printable interface and implement the method

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface for each person's detail
+ * Converts detail into printable string format
+ *
+ */
+public interface Printable {
+    String getPrintableString();
+}
