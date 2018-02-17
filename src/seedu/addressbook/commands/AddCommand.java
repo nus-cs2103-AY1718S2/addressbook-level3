1a7a541c1eb6b4703709768c16016bb2469ed1d0
MuhdNurKamal <mnkamal95@gmail.com>
2018-02-17 23:05:58 +0800

    Add isMutating methods to Command Classes

--- src/seedu/addressbook/commands/AddCommand.java
+++ src/seedu/addressbook/commands/AddCommand.java
@@ -66,5 +66,9 @@ public class AddCommand extends Command {
             return new CommandResult(MESSAGE_DUPLICATE_PERSON);
         }
     }
-
+    
+    @Override
+    public boolean isMutating() {
+        return true;
+    }
 }
