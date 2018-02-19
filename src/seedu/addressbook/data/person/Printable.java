fd3e29fe518f2b81bbc15688dfe79f1f437077e2
Yu Hong Herald <yuhongherald@gmail.com>
2018-02-19 14:09:41 +0800

    Name, Phone, Email and Address now implements Printable. Add comments for getPrintableString.

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,9 @@
 package seedu.addressbook.data.person;
 
 public interface Printable {
+    /**
+     * Returns a printable version of the object
+     * @return String representation of object
+     */
     public String getPrintableString();
 }
