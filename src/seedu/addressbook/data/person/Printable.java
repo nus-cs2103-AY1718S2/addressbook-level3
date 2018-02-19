1a0ea6b91fb77db287153282749d292f3eead602
LLercpp <yuanquan1995@gmail.com>
2018-02-19 22:13:41 +0800

    Add in Printalbe interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,12 @@
+package seedu.addressbook.data.person;
+
+/**
+ * This interface suggests the implementing object can provide a printable string;
+ */
+public interface Printable {
+    /**
+     * Returns a printable string of the object.
+     * @return Printable string
+     */
+    public String getPrintableString();
+}
