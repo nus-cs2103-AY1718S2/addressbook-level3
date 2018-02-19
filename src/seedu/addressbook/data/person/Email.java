bfa31878b9914d07ff9756079b41ead9ef69769c
tinyjy94 <tiny_jy@hotmail.com>
2018-02-19 21:09:27 +0800

    Add Printable Interface and callers

--- src/seedu/addressbook/data/person/Email.java
+++ src/seedu/addressbook/data/person/Email.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email implements Printable{
 
     public static final String EXAMPLE = "valid@e.mail";
     public static final String MESSAGE_EMAIL_CONSTRAINTS =
@@ -47,6 +47,11 @@ public class Email {
         return other == this // short circuit if same object
                 || (other instanceof Email // instanceof handles nulls
                 && this.value.equals(((Email) other).value)); // state check
+    }
+
+    @Override
+    public String getPrintableString() {
+        return "Email: " + value;
     }
 
     @Override
