7a938ee905df7a46ee2651b5a03a07d1d50afc14
Tan Heng Yeow <tanhengyeow@gmail.com>
2018-02-20 20:33:11 +0800

    Added implementation for getPrintableString in relevant classes

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -1,6 +1,7 @@
 package seedu.addressbook.ui;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.Printable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,6 +62,19 @@ public class Formatter {
      */
     private static String getIndexedListItem(int visibleIndex, String listItem) {
         return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+    /**
+     * Returns a concatenated version of the printable strings of each object.
+     */
+    public static String getPrintableString(Printable... printables){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Printable p : printables) {
+            stringBuilder.append(p.getPrintableString());
+        }
+
+        return stringBuilder.toString();
     }
 
 }
