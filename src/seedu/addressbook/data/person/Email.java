dde068f26f3feb43dd41884a83fe26fc0f5f4057
MuhdNurKamal <mnkamal95@gmail.com>
2018-02-20 21:13:47 +0800

    Make Email class implement Printable

--- src/seedu/addressbook/data/person/Email.java
+++ src/seedu/addressbook/data/person/Email.java
@@ -6,13 +6,15 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email implements Printable{
 
     public static final String EXAMPLE = "valid@e.mail";
     public static final String MESSAGE_EMAIL_CONSTRAINTS =
             "Person emails should be 2 alphanumeric/period strings separated by '@'";
     public static final String EMAIL_VALIDATION_REGEX = "[\\w\\.]+@[\\w\\.]+";
-
+    public static final String FORMAT_EMAIL_HEADER = "Address: ";
+    public static final String FORMAT_PRIVATE = "(private) ";
+    
     public final String value;
     private boolean isPrivate;
 
@@ -57,5 +59,14 @@ public class Email {
 
     public boolean isPrivate() {
         return isPrivate;
+    }
+
+    @Override
+    public String getPrintableString() {
+        String printableString = value;
+        if(isPrivate)
+            printableString = FORMAT_PRIVATE + printableString;
+        printableString = FORMAT_EMAIL_HEADER + printableString;
+        return printableString;
     }
 }
