70187e097397bc3bbef4c73d7cde3fa4b6464298
md-azsa <md.azsa@gmail.com>
2018-02-20 17:45:15 +0800

    Created Printable interface Classes Name, Email, Phone and Address implements Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface to get the values classes: Name, Phone, Email, Address
+ */
+public interface Printable {
+    /* Produces printable string representation */
+    public String getPrintableString();
+}
