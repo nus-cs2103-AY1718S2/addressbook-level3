52028d90bdc02191ff2e4f2d776cb768041564ed
Charmaine Lee <e0052642@u.nus.edu>
2018-02-21 02:29:46 +0800

    Add Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Interface of printable information
+ */
+public interface Printable {
+    /**
+     * Returns a printable string representation of the object.
+     */
+    String getPrintableString();
+}
