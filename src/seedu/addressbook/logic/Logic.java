8b9ce2b65039e199fb8c35e664acd4aedad81a0b
Shi Rong <shi_rong_1994@yahoo.com.sg>
2018-02-21 01:48:44 +0800

    LO-Polymorphism

--- src/seedu/addressbook/logic/Logic.java
+++ src/seedu/addressbook/logic/Logic.java
@@ -85,8 +85,14 @@ public class Logic {
     private CommandResult execute(Command command) throws Exception {
         command.setData(addressBook, lastShownList);
         CommandResult result = command.execute();
-        storage.save(addressBook);
+        saveResultToAddressBook(command);
         return result;
+    }
+
+    private void saveResultToAddressBook(Command command) throws StorageFile.StorageOperationException {
+        if(command.isMutating()) {
+            storage.save(addressBook);
+        }
     }
 
     /** Updates the {@link #lastShownList} if the result contains a list of Persons. */
