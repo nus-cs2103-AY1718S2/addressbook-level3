6a7ce3d1c3f7423465b207822baeda58e367c48a
Caijun7 <lohcaijun@gmail.com>
2018-02-20 22:32:33 +0800

    Added isMutating method to all commands class

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -70,4 +70,9 @@ public abstract class Command {
     public void setTargetIndex(int targetIndex) {
         this.targetIndex = targetIndex;
     }
+
+    /**
+     * Checks if the command executed mutates the data
+     */
+    public abstract boolean isMutating();
 }
