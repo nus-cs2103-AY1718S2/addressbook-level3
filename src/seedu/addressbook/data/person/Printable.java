fb90d7d6b9363e2f66553d0ecf11740d6482e587
jordancjq <jordancjq@hotmail.com>
2018-02-22 00:04:58 +0800

    Create Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+
+/**
+ * An interface to get the printable strings of an object
+ */
+public interface Printable {
+
+    String getPrintableString();
+
+}
