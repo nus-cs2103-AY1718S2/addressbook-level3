579ae3c56005ce098caca5b8cf4ae8a9ac7628cc
luca590 <lucasgaylord@protonmail.com>
2018-02-22 04:07:59 +0800

    implemented get PrintableString() in all classes

--- src/seedu/addressbook/data/person/Email.java
+++ src/seedu/addressbook/data/person/Email.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email implements Printable {
 
     public static final String EXAMPLE = "valid@e.mail";
     public static final String MESSAGE_EMAIL_CONSTRAINTS =
@@ -54,8 +54,16 @@ public class Email {
         return value.hashCode();
     }
 
-
     public boolean isPrivate() {
         return isPrivate;
     }
+
+    /**
+     * Inherits from Printable interface and gives printable string representation of object
+     */
+    @Override
+    public String getPrintableString(Printable... printables) {
+        return ("Email: " + this.value);
+    }
+
 }
