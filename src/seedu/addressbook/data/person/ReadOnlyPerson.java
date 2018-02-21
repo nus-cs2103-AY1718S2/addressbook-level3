8acc5f201f795bd576419be941106b4eaedb182e
KEVIN CHIN <kevinchinjh@gmail.com>
2018-02-21 21:04:23 +0800

    [W6.4h][W09-B2] Kevin Chin

--- src/seedu/addressbook/data/person/ReadOnlyPerson.java
+++ src/seedu/addressbook/data/person/ReadOnlyPerson.java
@@ -37,23 +37,7 @@ public interface ReadOnlyPerson {
      */
     default String getAsTextShowAll() {
         final StringBuilder builder = new StringBuilder();
-        final String detailIsPrivate = "(private) ";
-        builder.append(getName())
-                .append(" Phone: ");
-        if (getPhone().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getPhone())
-                .append(" Email: ");
-        if (getEmail().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getEmail())
-                .append(" Address: ");
-        if (getAddress().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getAddress())
+        builder.append(getPrintableString(getName(), getPhone(), getEmail(), getAddress()))
                 .append(" Tags: ");
         for (Tag tag : getTags()) {
             builder.append(tag);
@@ -81,5 +65,18 @@ public interface ReadOnlyPerson {
             builder.append(tag);
         }
         return builder.toString();
+    }
+
+    /**
+     * Returns a concatenated version of the printable strings of each object.
+     */
+    default String getPrintableString(Printable... printables){
+        StringBuilder sb = new StringBuilder();
+
+        for(Printable p: printables){
+            sb.append(p.getPrintableString() + " ");
+        }
+
+        return sb.toString().trim();
     }
 }
