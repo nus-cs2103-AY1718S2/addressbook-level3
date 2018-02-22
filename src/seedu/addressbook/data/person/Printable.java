ceeee4d7446f37df0bbfa90398fedc86c0d5b133
Chu Qing Hao <6337103+qinghao1@users.noreply.github.com>
2018-02-22 14:47:47 +0800

    Add Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,12 @@
+package seedu.addressbook.data.person;
+
+/** Printable interface for contact fields
+ *
+ */
+public interface Printable {
+    /**
+     * Get printable string of contact field
+     * @return printable string
+     */
+    public String getPrintableString();
+}
