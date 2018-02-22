1c2470f462c4c7a5d914c9270ba6f8214d41a8a4
DESKTOP-9K2NI32\Liu Yiming <e0031565@u.nus.edu>
2018-02-22 19:07:23 +0800

    adds Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface to print details of a person
+ */
+
+public interface Printable {
+
+    /**
+     * Get a printable string of the person's detail
+     * @return printable string
+     */
+    String getPrintableString();
+}
