83c11cce478aa6b02a1d6459203d305a2a00e26c
Cheryl Toh <e0035693@u.nus.edu>
2018-02-22 16:43:32 +0800

    Add header comment for Printable interface

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,8 @@
 package seedu.addressbook.data.person;
 
+/**
+ * An interface that aids in retrieving a printable string for an object
+ */
 public interface Printable {
 
     String getPrintableString();
