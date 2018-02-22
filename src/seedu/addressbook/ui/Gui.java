ed88e3096422262949e5ff7edfb69bdc60e3c2bf
DESKTOP-7ICV2HR\ongkc <ongkc95@gmail.com>
2018-02-22 23:26:02 +0800

    JavaFX

--- src/seedu/addressbook/ui/Gui.java
+++ src/seedu/addressbook/ui/Gui.java
@@ -2,6 +2,7 @@ package seedu.addressbook.ui;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.Main;
@@ -41,6 +42,9 @@ public class Gui {
          * More info: http://docs.oracle.com/javase/8/docs/technotes/guides/lang/resources.html#res_name_context
          */
         loader.setLocation(Main.class.getResource("ui/mainwindow.fxml"));
+
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../resource/icons/address_book_icon.png")));
+
 
         stage.setTitle(version);
         stage.setScene(new Scene(loader.load(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT));
