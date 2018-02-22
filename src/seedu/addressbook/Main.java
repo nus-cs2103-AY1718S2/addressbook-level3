dad9cd0d454a586e44eac7f21ca47cc61c3d238f
Cheryl Toh <e0035693@u.nus.edu>
2018-02-22 21:55:57 +0800

    Add icon to addressbook

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -3,6 +3,7 @@ package seedu.addressbook;
 import javafx.application.Application;
 import javafx.application.Platform;
 
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.ui.Gui;
@@ -22,6 +23,7 @@ public class Main extends Application implements Stoppable{
     public void start(Stage primaryStage) throws Exception{
         gui = new Gui(new Logic(), VERSION);
         gui.start(primaryStage, this);
+        primaryStage.getIcons().add(new Image("file:resources/images/addressbook_icon.png"));
     }
 
     @Override
