2b7df020bf138e42e149a15b5179da4dcff59084
USER-PC\User <ekabuyung@gmail.com>
2018-02-22 22:49:57 +0800

    line break to accomodate coding standard

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -37,7 +37,7 @@ public abstract class Command {
     }
 
     /**
-     *@return true if address book data is mutated.
+     * Returns true if address book data is mutated.
      */
     public abstract boolean isMutating();
 
