0fd25d7b4d7408f5bb361975f0d277ed5f7e6bec
Tey Xin Hui <e0052665@u.nus.edu>
2018-02-22 21:55:35 +0800

    create new Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+
+public interface Printable {
+    /**
+     *
+     * @return a concatenated version of a printable string of each object.
+     */
+    String getPrintableString();
+}
