29612e5e701b03f3701c395d3c43e8561c5b2a91
kengsengg <gks573@hotmail.com>
2018-02-23 16:29:14 +0800

    added Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface for producing a printable string representation of the object
+ */
+public interface Printable {
+    String getPrintableString();
+}
