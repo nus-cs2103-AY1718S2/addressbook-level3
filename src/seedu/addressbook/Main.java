5dfffd7b332479b3804b86f362fd0e34926c09d6
Goh Zu Wei <e0105028@u.nus.edu>
2018-02-24 02:20:42 +0800

    Add application icon

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -3,6 +3,7 @@ package seedu.addressbook;
 import javafx.application.Application;
 import javafx.application.Platform;
 
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.ui.Gui;
@@ -22,6 +23,8 @@ public class Main extends Application implements Stoppable{
     public void start(Stage primaryStage) throws Exception{
         gui = new Gui(new Logic(), VERSION);
         gui.start(primaryStage, this);
+        // Sets application icon
+        primaryStage.getIcons().add(new Image("file:resources/icons/addressBkIcon2.png"));
     }
 
     @Override
