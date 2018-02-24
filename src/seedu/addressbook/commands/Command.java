0188cfdf1ce228771059088808c5773393acdfbd
Lim Zheng Kai <limzk1994@hotmail.com>
2018-02-24 13:54:56 +0800

    abstract method should not have a body

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,7 +40,7 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute(){
-        throw new UnsupportedOperationException("This method should be implement in child classes");
+       // throw new UnsupportedOperationException("This method should be implement in child classes");
     }
 
     //Note: it is better to make the execute() method abstract, by replacing the above method with the line below:
