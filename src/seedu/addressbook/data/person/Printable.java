a27f22210eb3cf1b396790e93bbcba2554a94cb7
Colin Ng <colinn94@gmail.com>
2018-02-25 01:27:06 +0800

    Add Printable Interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+
+/**
+ * An interface to that lists Person details in a printable format
+ */
+public interface Printable {
+
+    String getPrintableString();
+
+}
