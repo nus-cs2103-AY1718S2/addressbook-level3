2ebca244981e50651231e6f163d291d978034f40
sherlynng <sherlyn.xinler@gmail.com>
2018-02-27 13:11:44 +0800

    Add header comments for Printable interface

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,8 @@
 package seedu.addressbook.data.person;
 
+/**
+ * An interface that formats how a person's details is represented.
+ */
 public interface Printable {
     String getPrintableString();
 }
