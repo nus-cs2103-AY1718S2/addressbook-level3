5433f32f229bc9eaa3363fc7077171ed0092df92
Sheikh Umar Bin Shaik Abu Bakar Banafe <sheikh.umar.banafe@gmail.com>
2018-03-03 15:21:34 +0800

    Revert "Updated findCommand to search for keywords in lowercase."
    This reverts commit c926e7ce5012b35a1ae33b5a8bf580d2413b0537.

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -13,17 +13,14 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n\t"
+            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n\t"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n\t"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
     private final Set<String> keywords;
 
-    /**
-     * Converts keywords that user entered into lowercase.
-     */
     public FindCommand(Set<String> keywords) {
-        this.keywords = keywords.toLowerCase();
+        this.keywords = keywords;
     }
 
     /**
@@ -48,7 +45,7 @@ public class FindCommand extends Command {
     private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
-            final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName().toLowerCase());
+            final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
             if (!Collections.disjoint(wordsInName, keywords)) {
                 matchedPersons.add(person);
             }
