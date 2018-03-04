7aa678f9487a81e7876e174b30454ab37c49e05d
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2018-03-04 17:33:59 +0800

    [Command.java] Refactor: add whitespace.

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -39,7 +39,7 @@ public abstract class Command {
     /**
      * Executes the command and returns the result.
      */
-    public CommandResult execute(){
+    public CommandResult execute() {
         throw new UnsupportedOperationException("This method should be implement in child classes");
     }
 
