5d8a3ce1c98c0627087059208250d581a1cb87ec
fishTT <1070450490@qq.com>
2018-03-05 21:05:34 +0800

    added sort function to UserGuide added sort to test case

--- test/java/seedu/addressbook/parser/ParserTest.java
+++ test/java/seedu/addressbook/parser/ParserTest.java
@@ -58,6 +58,12 @@ public class ParserTest {
         final String input = "list";
         parseAndAssertCommandType(input, ListCommand.class);
     }
+	
+	 @Test
+    public void listCommand_parsedCorrectly() {
+        final String input = "sort";
+        parseAndAssertCommandType(input, SortCommand.class);
+    }
 
     @Test
     public void exitCommand_parsedCorrectly() {
