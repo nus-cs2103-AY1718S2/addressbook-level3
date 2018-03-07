84cc95e83fc6fb6c00819e309db5ea7d2089f56c
Qiu Haohao <qiuh0008@e.ntu.edu.sg>
2018-03-07 15:49:18 +0800

    Add SortCommand Class

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,22 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import java.util.List;
+
+/**
+ * Sorts entries in the address book according to first name.
+ */
+
+public class SortCommand extends Command{
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts entries in the address book according to first name.\n"
+            + "Example: " + COMMAND_WORD;
+
+    @Override
+    public CommandResult execute() {
+        //To be implemented
+        return new CommandResult("To be implemented");
+    }
+}
