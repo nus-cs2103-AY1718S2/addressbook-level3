2dd7e483be613cfd6c34e6d13cd835ad7aee365b
traceurgan <traceur.gan@gmail.com>
2018-03-10 01:23:05 +0800

    Added functional SortComand class

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,7 +1,15 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.data.person.Person;
+
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
     //do something
+    @Override
+    public CommandResult execute() {
+        List<Person> allPerson = addressBook.getAllPersons().getInternalList();
+        allPerson.sort((Person p1, Person p2) -> (p1.getName().toString()).compareTo(p2.getName().toString()));
+        return new CommandResult(getMessageForPersonListShownSummary(allPerson), allPerson);
+    }
 }
